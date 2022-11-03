aedac3c4aa0c56650ab7cf98c1a289a1c82992eb
PawanVedpathak <80150887+Vepathakpawan@users.noreply.github.com>
2022-11-03 13:41:53 +0530

    card with slider

--- src/component/card.js
+++ src/component/card.js
@@ -1,17 +1,54 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import Slider from "react-slick";
 
 import CardData from "./data/CardData";
 
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+
 const Cards = (props) => {
 
-  const [noOfElement, setnoOfElement]= useState(6);
+  const [noOfElement, setnoOfElement]= useState(9);
   const showMore = () =>{
     setnoOfElement(noOfElement + noOfElement);
   }
 
   const slice = CardData.slice(0,noOfElement);
-
+  const settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  };
   return (
     <>
       <div className="card-section">
@@ -20,22 +57,25 @@ const Cards = (props) => {
             <h2>JavaScript Coding</h2>
             <div className="row card-layout">
   
-
+            <Slider {...settings}>
             {slice.map((value, index ) => {
               return <>
-                <Link  className="card" to={value.path} key={index} >
+               
+              <div  className="card">
+                <Link  to={value.path} key={index} >
                <img src={value.image} className="card-img"/>
                   <div className="card-intro">
                     <h3 className="hv">{value.title}</h3>
                     <p className="hv">{value.desc}</p>
                   </div>
                 </Link>
-       
+                </div>
+         
               </>;
             })}
-
+       </Slider>
           </div>
-          <button className="btn show-more" onClick ={showMore}>  Show more</button>
+          {/* <button className="btn show-more" onClick ={showMore}>  Show more</button> */}
 
         </div>
         </div>
